cdfec6011a3955f1a0b5026f2517fbb331f3d600
andriy <andriy@intteh.com>
2025-02-14 18:53:15 +0200

    add multiple models evaluation

--- experiments/evaluation/run_eval.sh
+++ experiments/evaluation/run_eval.sh
@@ -9,23 +9,27 @@ fi
 echo "Using device: $DEVICE"
 echo "Using splits: $SPLITS"
 echo "Number of samples per split: $NUM_SAMPLES_PER_SUBSET"
-echo "Using model: $MODEL"
+echo "Using model: $MODELS"
 
 # Define splits
 IFS=',' read -r -a SPLIT_ARRAY <<< "$SPLITS"
+IFS=',' read -r -a MODEL_ARRAY <<< "$MODELS"
 
-for SPLIT in "${SPLIT_ARRAY[@]}"; do
-  echo "Processing split: $SPLIT"
-  sed -i "s|VALIDATION_SPLIT_PLACEHOLDER|$SPLIT|g" /workspace/config.yaml
-  OUTPUT_DIR="/workspace/output/${SPLIT}"
+for MODEL in "${MODEL_ARRAY[@]}"; do
+  for SPLIT in "${SPLIT_ARRAY[@]}"; do
+    echo "Processing model: $MODEL"
+    echo "Processing split: $SPLIT"
+    sed -i "s|VALIDATION_SPLIT_PLACEHOLDER|$SPLIT|g" /workspace/config.yaml
+    OUTPUT_DIR="/workspace/output/${SPLIT}/${MODEL}"
 
-  lm_eval \
-    --model hf \
-    --device "$DEVICE"\
-    --model_args pretrained="$MODEL" \
-    --include_path ./ \
-    --tasks config \
-    --output "$OUTPUT_DIR" \
-    --limit "$NUM_SAMPLES_PER_SUBSET" \
-    --log_samples
+    lm_eval \
+      --model hf \
+      --device "$DEVICE"\
+      --model_args pretrained="$MODEL" \
+      --include_path ./ \
+      --tasks config \
+      --output "$OUTPUT_DIR" \
+      --limit "$NUM_SAMPLES_PER_SUBSET" \
+      --log_samples
+  done
 done
